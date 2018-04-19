0fe2132f53c70ca9996806c078d193d24febfe78
Freddie Ridell <freddie@codogo.io>
2018-04-19 09:46:07 +0100

    with ALL DONE button

--- src/App.js
+++ src/App.js
@@ -1,50 +1,55 @@
-import React, { Fragment } from "react";
-import component from "omniscient";
+import React, {Fragment} from 'react';
+import component from 'omniscient';
 
 import {
-  structureTodos,
   loadTodo,
+  setAllTodosDone,
+  setTodoText,
+  structureTodos,
   toggleTodoDone,
-  setTodoText
-} from "./structureTodos";
+} from './structureTodos';
 
 loadTodo(1);
 loadTodo(2);
 loadTodo(3);
 
-const Todo = component(({ todo }) => (
+const Todo = component(({todo}) => (
   <div
     style={{
-      margin: "1em",
-      backgroundColor: todo.get("done") ? "coral" : "lightgrey"
-    }}
-  >
-    {todo.get("loading") ? (
-      "loading..."
+      margin: '1em',
+      backgroundColor: todo.get('done') ? 'coral' : 'lightgrey',
+    }}>
+    {todo.get('loading') ? (
+      'loading...'
     ) : (
       <Fragment>
         <div onClick={() => toggleTodoDone(todo)}>
-          {todo.get("done") ? "done" : "not done"}
+          {todo.get('done') ? 'done' : 'not done'}
         </div>
         <input
           onChange={e => setTodoText(todo)(e.target.value)}
-          value={todo.get("text")}
+          value={todo.get('text')}
         />
       </Fragment>
     )}
   </div>
 ));
 
-const Todos = component(({ cursor }) => (
-  <div style={{ backgroundColor: "red", padding: "1em" }}>
-    {cursor.map(todo => <Todo key={todo.get("id")} todo={todo} />)}
+const Todos = component(({cursor}) => (
+  <div style={{backgroundColor: 'red', padding: '1em'}}>
+    {cursor.map(todo => <Todo key={todo.get('id')} todo={todo} />)}
+    <div
+      onClick={() => setAllTodosDone(cursor)}
+      style={{backgroundColor: 'green'}}>
+      ALL DONE
+    </div>
   </div>
 ));
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    structureTodos.on("swap", () => this.forceUpdate());
+    structureTodos.on('swap', () => this.forceUpdate());
   }
 
   render() {
